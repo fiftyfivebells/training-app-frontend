0f6cdf6954acf661951c17e354e7003ed3d5db96
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-11-07 19:36:40 -0500

    Export style files one by one instead of in an object

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,3 @@
-import { colors } from './colors';
-import { spacing } from './spacing';
-import { typography } from './typography';
-
-export const theme = {
-  colors,
-  spacing,
-  typography
-} as const;
-
-export type Theme = typeof theme;
+export { colors } from './colors';
+export { spacing } from './spacing';
+export { typography } from './typography';
