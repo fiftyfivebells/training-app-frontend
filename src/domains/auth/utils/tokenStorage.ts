b3577cf729b11ac1dc45647e923fa7663bdbd70b
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-11-23 16:25:30 -0500

    Add refresh token options to tokenStorage

--- src/domains/auth/utils/tokenStorage.ts
+++ src/domains/auth/utils/tokenStorage.ts
@@ -5,15 +5,25 @@ import {
 } from '@lib/storage/secureStorage'
 
 const ACCESS_TOKEN_KEY = 'auth.accessToken'
+const REFRESH_TOKEN_KEY = 'auth.refreshToken'
 
 export const tokenStorage = {
   async setAccessToken(accessToken: string): Promise<void> {
     await setSecureValue(ACCESS_TOKEN_KEY, accessToken)
   },
+  async setRefreshToken(refreshToken: string): Promise<void> {
+    await setSecureValue(REFRESH_TOKEN_KEY, refreshToken)
+  },
   async getAccessToken(): Promise<string | null> {
     return await getSecureValue(ACCESS_TOKEN_KEY)
   },
+  async getRefreshToken(): Promise<string | null> {
+    return await getSecureValue(REFRESH_TOKEN_KEY)
+  },
   async deleteAccessToken(): Promise<void> {
     await deleteSecureValue(ACCESS_TOKEN_KEY)
+  },
+  async deleteRefreshToken(): Promise<void> {
+    await deleteSecureValue(REFRESH_TOKEN_KEY)
   },
 }
