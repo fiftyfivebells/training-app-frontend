ae1588b48b2322d63918fb92d5cbd054ab9c2689
Stephen Bell <24374570+fiftyfivebells@users.noreply.github.com>
2025-11-24 20:41:09 -0500

    Return void when trying to store refresh tokens on web

--- src/domains/auth/utils/tokenStorage.ts
+++ src/domains/auth/utils/tokenStorage.ts
@@ -3,6 +3,7 @@ import {
   getSecureValue,
   setSecureValue,
 } from '@lib/storage/secureStorage'
+import { Platform } from 'react-native'
 
 const ACCESS_TOKEN_KEY = 'auth.accessToken'
 const REFRESH_TOKEN_KEY = 'auth.refreshToken'
@@ -12,18 +13,21 @@ export const tokenStorage = {
     await setSecureValue(ACCESS_TOKEN_KEY, accessToken)
   },
   async setRefreshToken(refreshToken: string): Promise<void> {
+    if (Platform.OS === 'web') return
     await setSecureValue(REFRESH_TOKEN_KEY, refreshToken)
   },
   async getAccessToken(): Promise<string | null> {
     return await getSecureValue(ACCESS_TOKEN_KEY)
   },
   async getRefreshToken(): Promise<string | null> {
+    if (Platform.OS === 'web') return null
     return await getSecureValue(REFRESH_TOKEN_KEY)
   },
   async deleteAccessToken(): Promise<void> {
     await deleteSecureValue(ACCESS_TOKEN_KEY)
   },
   async deleteRefreshToken(): Promise<void> {
+    if (Platform.OS === 'web') return
     await deleteSecureValue(REFRESH_TOKEN_KEY)
   },
 }
